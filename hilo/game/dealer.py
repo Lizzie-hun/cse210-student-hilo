f22fb2df384dda2922d80e031a1d758dbcdafcd0
= <bentley.rebekah314@gmail.com>
2021-10-08 11:27:53 -0600

    Hope this works

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -9,16 +9,8 @@ class Dealer():
         self.score = 300
         self.card = []
         self.flip = []
-<<<<<<< HEAD
         self.guess = []
-
-    #def can_flip(self):
-        #return(self.flip.count(300)>= 0)
-            #if yes continue playing and flip a card, if no, end the game.
-
-=======
         
->>>>>>> ed26b1f85cab66d6f7f87daedcc0bd33f2c402f0
 
 def get_points(self):
 
@@ -35,4 +27,3 @@ def flip_card(self):
         self.card.append(result)
 
     
->>>>>>> ed26b1f85cab66d6f7f87daedcc0bd33f2c402f0
