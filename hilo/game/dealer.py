64a5da352b69bab6fa3bd12e391c48e36a1b0fee
= <bentley.rebekah314@gmail.com>
2021-10-09 11:08:56 -0600

    small changes

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,25 +1,28 @@
-#file for dealer
-
-#Draw Cards and Get Points, class = Dealer, 
 import random
 
 class Dealer():
+    """
+    This does something
+    """
 
     def __init__(self):
         self.score = 0
         self.flipped_card = 0
         self.current_card = 0
-        self.next_card = 0
+        #self.previous_card = 0
         self.guess = ""
-    
+        self.previous_card = self.flipped_card
+
     def get_guess(self):
         self.guess = input("Higher or lower? [h/l] ")
-        
-
-        #This needs to add 100 points to the total score and subtract 75 for each wrong guess
-        #Once the score reaches 0 the game is over
 
     def flip_card(self):
         self.current_card = random.randint(1, 13)
-        self.flipped_card = self.current_card
-        return self.flipped_card
+        #self.current_card = self.previous_card
+        #self.flipped_card = self.current_card
+        self.flipped_card = random.randint(1,13)
+        #self.previous_card = self.flipped_card
+        #return self.current_card
+
+    #self.previous = flip_card
+
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -23,9 +23,9 @@ class Director:
         self.score = 0
         self.total_score = 300
         self.guess = ""
-        self.next_card = 0
-        self.flipped_card = 0
         self.dealer = Dealer()
+        self.next_card = 0
+        self.previous_card = 0
 
     def start_game(self):
         """Starts the game loop to control the sequence of play.
@@ -34,10 +34,11 @@ class Director:
             self (Director): an instance of Director.
         """
         while self.keep_playing:
-            self.dealer.flip_card()
-            self.do_updates()
             self.do_outputs()
             self.is_game_over()
+            self.do_updates()
+            self.is_game_over()
+            
 
     def is_game_over(self):
         """
@@ -47,13 +48,13 @@ class Director:
             self (Director): an instance of Director.
             score: Total score
         """
-        if self.total_score >= 0:
+        if self.total_score <= 0:
             print('Game over')
-            self.keep_playing == False
+            self.keep_playing = False
 
     def get_points(self):
         while self.total_score > 0:
-            if self.dealer.flipped_card > self.next_card and self.dealer.guess == "l" or self.dealer.flipped_card < self.next_card and self.dealer.guess == "h":
+            if self.dealer.current_card > self.dealer.flipped_card and self.dealer.guess == "l" or self.dealer.current_card < self.dealer.flipped_card and self.dealer.guess == "h":
                 self.score = 100
             else:
                 self.score = -75
@@ -62,14 +63,21 @@ class Director:
     #updates score
     def do_updates(self):
         points = self.get_points()
+        if points == None:
+            self.total_score = 0
+            print(self.total_score)
+            print('Game Over')
         self.total_score += points
     
     #outputs information for the game to the user
     def do_outputs(self):
-        print(f"\nThe card is: {self.dealer.flip_card()}")
+        #self.dealer.flip_card()
+        #self.next_card = self.dealer.flipped_card
+        self.previous_card = int(self.dealer.previous_card)
+        print(f"\nThe card is: {self.previous_card}")
         self.dealer.get_guess()
-        self.next_card = self.dealer.flip_card()
-        print(f"Next card was: {self.next_card}")
+        print(f"Next card was: {int(self.dealer.flipped_card)}")
+        #self.dealer.current_card = self.dealer.flipped_card
         print(f"Your score is: {self.total_score}")
         if self.keep_playing:
             choice = input("Draw again? [y/n] ")
